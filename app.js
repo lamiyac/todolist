81ae36c8ff5f98b08e00ef34909d00e109f003f8
Your Name <lamiyalami677@gmail.com>
2022-05-31 12:23:46 +0530

    last commit

--- app.js
+++ app.js
@@ -4,30 +4,40 @@
 const express= require("express");
 const bodyParser=require("body-parser");
 const { response } = require("express");
+const date=require(__dirname+ "/date.js");
 
 const app =express();
-let items=["Buy food","Cook food","Eat food"];
+const items=["Buy food","Cook food","Eat food"];
+const workItems=[];
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",function(request,response){
-let today =new Date();  
-let options ={
-    weekday:"long",
-    day:"numeric",
-    month:"long"
-};
-let day=today.toLocaleDateString("en-US",options);
-
-response.render("list", {KindOfDay: day,newListItems:items});
+    const day= date.getDate();
+response.render("list", {listTitle: day,newListItems:items});
+});
+app.get("/work",function(request,response){
+    response.render("list", {listTitle:"work list",newListItems:workItems});
+});
+app.get("/about",function(request,response){
+    response.render("about");
 });
 app.post("/",function(request,response){
-let item= request.body.newItem;
-items.push(item);
-response.redirect("/");
+    const item= request.body.newItem;
+    if(request.body.list === "work"){
+workItems.push(item);
+response.redirect("/work");
+    }else{
+        items.push(item);
+        response.redirect("/");
+    }
+
+
+
 });
+
 
 app.listen(3000,function(){
    
